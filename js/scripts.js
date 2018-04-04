1c5252f9168651ef0e8ae8933c8aecc33458d643
Eva Antipina and Rio Atmadja <rio@>
2018-04-04 13:11:43 -0700

    Working on UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 // Business logic
 
 
-function User(name, diceRoll, score, turn) {
-  this.name = name;
+function User(diceRoll, score, turn) {
   this.diceRoll = diceRoll;
   this.score = score;
   this.turn = turn;
@@ -20,24 +19,52 @@ User.prototype.turnScore = function(num) {
 
 // UI logic
 $(document).ready(function(){
-  $("#start").click(function(){
+  $("#start").on("click", function(){
 
     // initial values
     var name = "";
     var score = 0;
-    var turn = true;
+    var turn = false;
     var dice = 0;
 
     // instantiate users object
-    var newUser = new User(name, dice, score, turn);
-    var computer = new User(name, dice, score, turn);
+    var newUser = new User(dice, score, turn);
+    var computer = new User(dice, score, turn);
+    var random = 0;
 
+    while(newUser.score <= 100 || computer.score <= 100){
+      turn = $("#stop").on("click",function(){
+        turn = true;
+        console.log(turn);
+        return turn;
+      });
+      while(random !== 1 || turn){
+        console.log("Dice: " +random);
+        newUser.dice = newUser.turnScore(random);
+        console.log(dice);
+        random = Math.floor(Math.random() * 6) + 1;
+      }
+      if(random == 1){
+        newUser.dice = 0;
+      }
+      newUser.score = newUser.totalScore();
+      console.log("Score:" + newUser.score);
+      if(turn){
+        var i = 1;
+        while(random !== 1 || i <= 3){
+          console.log("Computer Dice: " +random);
+          computer.dice = computer.turnScore(random);
+          console.log(computer.dice);
+          random = Math.floor(Math.random() * 6) + 1;
+          i++;
+        }
+        if(random == 1){
+          dice = 0;
+        }
+        computer.score = computer.totalScore();
+        console.log("Computer Score:" + computer.score);
+      }
+    }
 
-    $('#roll').click(function() {
-
-       while(){
-         dice = Math.floor(Math.random() * 6);
-         newUser.turnScore(dice);
-       });
-     });
+  });
 })
